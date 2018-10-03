9ea4c7fc9df9e44fa2d39034b480efadbfa5a285
huanghantao <2812240764@qq.com>
2018-10-03 14:19:57 +0800

    Fix: fix the bug

--- src/http/hht_http.c
+++ src/http/hht_http.c
@@ -39,8 +39,8 @@ hht_http_header_node_t *new_http_header_node(unsigned char *key, unsigned char *
     http_header_node = (hht_http_header_node_t *)calloc(1, sizeof(*http_header_node));
     if (http_header_node != NULL) {
         INIT_LIST_HEAD(&(http_header_node->node));
-        http_header_node->key = hht_str_setto(key, sizeof(key) - 1);
-        http_header_node->value = hht_str_setto(value, sizeof(value) - 1);
+        http_header_node->key = hht_str_setto(key, strlen(key));
+        http_header_node->value = hht_str_setto(value, strlen(value));
     }
 
     return http_header_node;
@@ -55,7 +55,7 @@ hht_http_request_t *new_http_request(void)
         http_request->method = hht_str_setto(DEFAULT_METHOD, sizeof(DEFAULT_METHOD) - 1);
         http_request->path = hht_str_setto(DEFAULT_PATH, sizeof(DEFAULT_PATH) - 1);
         http_request->protocol = hht_str_setto(DEFAULT_PROTOCOL, sizeof(DEFAULT_PROTOCOL) - 1);
-        http_request->headers_in_list = new_http_header_node("Host: ", "localhost");
+        http_request->headers_in_list = new_http_header_node("", "");
         if (http_request->headers_in_list == NULL) {
             free(http_request);
             return NULL;
@@ -66,15 +66,19 @@ hht_http_request_t *new_http_request(void)
     return http_request;
 }
 
-void http_header_node_add(hht_http_request_t *http_request, unsigned char *key, unsigned char *value)
+void http_header_node_add(hht_http_request_t *http_request_o, unsigned char *key, unsigned char *value)
 {
     hht_http_header_node_t *http_header_node = new_http_header_node(key, value);
     if (http_header_node != NULL) {
-        list_add_tail(&(http_request->headers_in_list->node), &(http_header_node->node));
+        list_add_tail(&(http_request_o->headers_in_list->node), &(http_header_node->node));
     }
 }
 
-void http_header_node_each(hht_http_request_t *http_request, void (*handler)(void *node))
+void http_header_node_each(hht_http_request_t *http_request_o, void (*handler)(void *node))
 {
-
+    hht_list_head_t *head_node = &(http_request_o->headers_in_list->node);
+    hht_list_head_t *pos = head_node->next;
+    for (; pos != (head_node); pos = pos->next) {
+        handler(pos);
+    }
 }

68bfe5d1ddde2d0c2a1060881c8fa974a781124d
codinghuang <2812240764@qq.com>
2018-10-18 20:31:04 +0800

    Feat: parse the host

--- src/core/parse.c
+++ src/core/parse.c
@@ -5,13 +5,15 @@
 
 int parse_url(hht_str_t *url_str, hht_http_request_t *http_request, unsigned int *port)
 {
-    hht_str_t *tmp_url = url_str;
+	hht_str_t key_str = hht_str_setto("Host", 4);
+	hht_http_header_node_t *http_header_node;
 
 	if (url_str->len > 7 && hht_str_eqn_cstr(url_str, "http://", 7) == 0) {
-		tmp_url->data += 7;
+		url_str->data += 7;
+		url_str->len -= 7;
+		http_header_node = find_http_header_node_by_key(http_request, &key_str);
+		http_header_node->value = *url_str;
 	}
 
-    write(1, tmp_url->data, tmp_url->len - 7);
-	
 	return 0;
 }

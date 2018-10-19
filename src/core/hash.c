86b5c719ad0a72b4440e8801b0f9bf93d0d29904
codinghuang <2812240764@qq.com>
2018-10-19 15:37:09 +0800

    Feat: add hash.h

---
+++ src/core/hash.c
@@ -0,0 +1,28 @@
+#include "hash.h"
+
+uint64_t hash_func(char *key, size_t len)
+{
+    uint64_t hash = 5381;
+
+    for (; len >= 8; len -= 8) {
+        hash = ((hash << 5) + hash) + *key++;
+        hash = ((hash << 5) + hash) + *key++;
+        hash = ((hash << 5) + hash) + *key++;
+        hash = ((hash << 5) + hash) + *key++;
+        hash = ((hash << 5) + hash) + *key++;
+        hash = ((hash << 5) + hash) + *key++;
+        hash = ((hash << 5) + hash) + *key++;
+        hash = ((hash << 5) + hash) + *key++;
+    }
+    switch (len) {
+        case 7: hash = ((hash << 5) + hash) + *key++;
+        case 6: hash = ((hash << 5) + hash) + *key++;
+        case 5: hash = ((hash << 5) + hash) + *key++;
+        case 4: hash = ((hash << 5) + hash) + *key++;
+        case 3: hash = ((hash << 5) + hash) + *key++;
+        case 2: hash = ((hash << 5) + hash) + *key++;
+        case 1: hash = ((hash << 5) + hash) + *key++; break;
+        case 0: break;
+    }
+    return hash;
+}
---
+++ src/core/hash.h
@@ -0,0 +1,11 @@
+#ifndef __HHT_HASH_H__
+#define __HHT_HASH_H__
+
+
+#include <stdlib.h>
+#include <stdint.h>
+
+
+uint64_t hash_func(char *key, size_t len);
+
+#endif /* __HHT_HASH_H__ */

6114e9a9e641a7de470187b1d023498ca6a6f7be
codinghuang <2812240764@qq.com>
2018-10-21 16:00:46 +0800

    Modify: modify the hht_parse_option

--- src/core/hht_opt.h
+++ src/core/hht_opt.h
@@ -12,5 +12,5 @@ typedef struct hht_opt_s {
 } hht_opt_t;
 
 int hht_parse_option(int argc, char * const *argv, hht_opt_t *opt_o);
-
+void upper_method_name(hht_str_t *str);
 #endif /* __HHT_OPT_H__ */

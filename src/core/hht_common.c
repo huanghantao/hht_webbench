ae4dd540d2d2e27f0349e355dd2c54a421065952
codinghuang <2812240764@qq.com>
2018-10-24 20:52:21 +0800

    Modify: modify the hht_opt_s structure

--- src/core/hht_common.c
+++ src/core/hht_common.c
@@ -5,9 +5,9 @@ void usage(void)
 {
     fprintf(stderr,
             "hht_webbench [option] ... URL\n"
-            "  -t <sec>          Limit the maximum time for testing (unit s). Default: no time limit.\n"
-            "  -c                Number of clients. Default four. Default: 4\n"
-            "  -n                Total number of requests for all clients. Default: 1000\n"
+            "  -t                Total number of threads.\n"
+            "  -c                Total number of HTTP connections to keep open\n"
+            "  -d <sec>          Limit the maximum time for testing (unit s). Default: 10s.\n"
             "  -m                Use request method. Default: GET\n"
             "  -h                Show how the hht_webbench is used.\n"
             "  -V                Display hht_webbench version.\n"
--- src/core/hht_opt.h
+++ src/core/hht_opt.h
@@ -2,17 +2,21 @@
 #define __HHT_OPT_H__
 
 
+#include <stdint.h>
 #include "../config/hht_config.h"
 #include "hht_string.h"
 #include "hht_include.h"
 
 struct hht_opt_s {
-    unsigned int client_n;
-    unsigned int request_n;
+    uint64_t threads;
+    uint64_t connections;
+    uint64_t duration;
     hht_str_t method;
+    hht_str_t url;
 };
 
 hht_opt_t *new_hht_opt();
 int hht_parse_option(int argc, char * const *argv, hht_opt_t *opt_o);
 void upper_method_name(hht_str_t *str);
+void print_opts(hht_opt_t *opt_o);
 #endif /* __HHT_OPT_H__ */
